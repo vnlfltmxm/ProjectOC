2af830f1c951fa580fd263a6e3a9d2e2e36dfbfc
박상욱 <qkr3871@gmail.com>
2024-10-02 09:38:44 +0900

    [Add] 플레이어가 아이템을 골랐을때 아이템의 위치를 플레이어의 손으로 옮기는 함수 추가

--- Assets/PSW/Script/ItemBase.cs
+++ Assets/PSW/Script/ItemBase.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public abstract class ItemBase : MonoBehaviour,IItem
 {
+    public virtual void ChoiceItem(Transform playerHandTransform)
+    {
+        if (playerHandTransform == null) 
+        { 
+            return;
+        }
 
+        if (this.gameObject.activeSelf == false)
+        {
+            this.gameObject.SetActive(true);
+        }
+        this.gameObject.transform.parent = playerHandTransform;
+        this.gameObject.transform.position = playerHandTransform.position;
+    }
 }
