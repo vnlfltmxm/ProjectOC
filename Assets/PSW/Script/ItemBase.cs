5829927d61ad5e2612cbcbb65e2486b2d603534d
박상욱 <qkr3871@gmail.com>
2024-10-02 13:38:47 +0900

    [Add] 아이템 매니저 추가중

--- Assets/PSW/Script/ItemBase.cs
+++ Assets/PSW/Script/ItemBase.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public abstract class ItemBase : MonoBehaviour,IItem
 {
+    protected new Rigidbody rigidbody;
+
+    public virtual void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public virtual void PickUpItem()
+    {
+        //플레이어 인벤에 추가하는 기능
+    }
+
     public virtual void ChoiceItem(Transform playerHandTransform)
     {
         if (playerHandTransform == null) 
@@ -15,7 +27,14 @@ public abstract class ItemBase : MonoBehaviour,IItem
         {
             this.gameObject.SetActive(true);
         }
+        rigidbody.useGravity = false;
         this.gameObject.transform.parent = playerHandTransform;
         this.gameObject.transform.position = playerHandTransform.position;
+    }
+
+    public virtual void DropItem()
+    {
+        this.gameObject.transform.parent = null;
+        rigidbody.useGravity = true;
     }
 }
