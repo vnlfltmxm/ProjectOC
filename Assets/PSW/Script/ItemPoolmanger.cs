1f450e2f4a85285b59b90f8a17731ff76f54caa6
박상욱 <qkr3871@gmail.com>
2024-10-04 10:59:59 +0900

    [Add] 풀매니저 추가 및 풀 세팅 기능 추가

---
+++ Assets/PSW/Script/ItemPoolmanger.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPoolmanger : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] prefabs;
+
+    private Dictionary<string, Dictionary<int, GameObject>> itemPool = new Dictionary<string, Dictionary<int, GameObject>>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void SetPool()
+    {
+        //for (int i = 0; i < prefabs.Length; i++)
+        //{
+        //    int index = 0;
+        //    for (int j = 0; j < DataManger.inst.GetItem(prefabs[i].name).count; j++)
+        //    {
+        //        GameObject item = Instantiate(prefabs[i]);
+        //        item.SetActive(false); 
+
+        //        int tempId = DataManger.inst.GetItem(prefabs[i].name).id + index;
+
+        //        if (itemPool.ContainsKey(prefabs[i].name) == false) 
+        //        {
+        //            Dictionary<int, GameObject> tempDic = new Dictionary<int, GameObject>();
+        //            tempDic.Add(tempId, item);
+        //            itemPool.Add(prefabs[i].name, tempDic);
+        //        }
+        //        else
+        //        {
+        //            itemPool[prefabs[i].name].Add(tempId, item);
+        //        }
+
+        //        index++;
+        //    }
+        //}
+    }
+
+    public void AddItemInPool()
+    {
+        
+    }
+}
